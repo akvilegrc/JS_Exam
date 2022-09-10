e3be9a2366116ee40f0ebfcfdf008465abb3b78a
Akvile Griciute <akvile.griciute@codeacademylt.onmicrosoft.com>
2022-09-10 19:19:44 +0300

    task 1 tbc

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,30 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const output = document.getElementById("output");
+const form = document.querySelector("form");
+const kilogramsInput = document.getElementById("search");
+
+form.addEventListener("submit", converterFunction);
+
+function converterFunction(event){
+    event.preventDefault();
+    const kgs = Number(kilogramsInput.value);
+    const lbs = kgs * 2.2046;
+    const grams = kgs / 0.0010000;
+    const oz = kgs * 35.274;
+
+    const lbsConversion = document.createElement("p");
+    const gramsConversion = document.createElement("p");
+    const ozConversion = document.createElement("p");
+
+    lbsConversion.textContent = `${kgs}kg = ${lbs}lbs`;
+    gramsConversion.textContent = `${kgs}kg = ${grams}g`;
+    ozConversion.textContent = `${kgs}kg = ${oz}oz`;
+
+    output.appendChild(lbsConversion);
+    output.appendChild(gramsConversion);
+    output.appendChild(ozConversion);
+
+}

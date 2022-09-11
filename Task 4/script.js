ea0fb1a8469b0019f577ebacc3408e20a6426c29
Akvile Griciute <akvile.griciute@codeacademylt.onmicrosoft.com>
2022-09-11 10:34:48 +0300

    task 4 styling

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,32 +9,31 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
 
 const response = fetch(ENDPOINT);
 const output = document.getElementById("output");
 
 response
-    .then((response) => response.json())
-    .then((data) => {
-        data.forEach((car) => {
-            const carBrand = car.brand;
-            const card = document.createElement("div");
-            card.setAttribute("id","wrapper-card");
-            const brandName = document.createElement("h2");
-            const brandModels = document.createElement("ul");
-            output.appendChild(card);
-            card.appendChild(brandName);
-            
-            brandName.textContent = carBrand;
-            const allModels = car.models;
-            allModels.forEach((model) => {
-                const oneModel = document.createElement("li");
-                oneModel.textContent = model;
-                brandModels.appendChild(oneModel)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((car) => {
+      const carBrand = car.brand;
+      const card = document.createElement("div");
+      card.setAttribute("id", "wrapper-card");
+      const brandName = document.createElement("h2");
+      const brandModels = document.createElement("ul");
+      output.appendChild(card);
+      card.appendChild(brandName);
 
-            })
-            card.appendChild(brandModels);
+      brandName.textContent = carBrand;
+      const allModels = car.models;
+      allModels.forEach((model) => {
+        const oneModel = document.createElement("li");
+        oneModel.textContent = model;
+        brandModels.appendChild(oneModel);
+      });
+      card.appendChild(brandModels);
+    });
+  });
 
-        })
-    })
